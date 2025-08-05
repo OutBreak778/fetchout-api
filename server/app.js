495d0462c18f30c64476e9917b62efa93cfb0a60
OutBreak778 <mishran.2003@gmail.com>
2025-08-05 20:56:10 +0530

    trying to deploy backend

--- server/app.js
+++ server/app.js
@@ -1,41 +1,46 @@
-import express from "express"
-import dotenv from "dotenv"
-import helmet from "helmet"
-import morgan from "morgan"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import routes from "./routes/index.js"
-import errorHandler from "./middleware/errorHandler.js"
-import authRoutes from "./routes/authRoutes.js"
-import apiEndpointRoutes from "./routes/apiEndpointRoute.js"
-import usageLogRoutes from "./routes/usageLogRoute.js"
-import paramsRoutes from "./routes/paramsRoute.js"
-import dashboardRoutes from "./routes/dashboardRoute.js"
-import userRoutes from "./routes/userRoute.js"
+import routes from "./routes/index.js";
+import errorHandler from "./middleware/errorHandler.js";
+import authRoutes from "./routes/authRoutes.js";
+import apiEndpointRoutes from "./routes/apiEndpointRoute.js";
+import usageLogRoutes from "./routes/usageLogRoute.js";
+import paramsRoutes from "./routes/paramsRoute.js";
+import dashboardRoutes from "./routes/dashboardRoute.js";
+import userRoutes from "./routes/userRoute.js";
 
-dotenv.config({quiet: true})
-const app = express()
+dotenv.config({ quiet: true });
+const app = express();
 
 // Middleware
-app.use(helmet())
-app.use(morgan("dev"))
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 //Routes
-app.use("/api/v1", routes)
-app.use("/auth", authRoutes)
-app.use("/auth/user", userRoutes)
-app.use("/api/endpoint", apiEndpointRoutes)
-app.use("/api/usage-log", usageLogRoutes)
-app.use("/api", paramsRoutes)
-app.use("/dashboard", dashboardRoutes)
+app.use("/api/v1", routes);
+app.use("/auth", authRoutes);
+app.use("/auth/user", userRoutes);
+app.use("/api/endpoint", apiEndpointRoutes);
+app.use("/api/usage-log", usageLogRoutes);
+app.use("/api", paramsRoutes);
+app.use("/dashboard", dashboardRoutes);
 
 //Errors
-app.use(errorHandler)
+app.use(errorHandler);
 
-export default app
+export default app;
